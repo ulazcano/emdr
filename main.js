1b70b26a2a2fd3eb02db35825b525c466f1d7ae1
uliseslazcano <uliseslazcano@gmail.com>
2022-11-30 12:31:16 -0300

    al fin ctm

--- main.js
+++ main.js
@@ -1,95 +1,143 @@
 //collect data from form
 //let speed_loop_post = document.querySelector("#speed").value;
-//console.log(speed_loop_post);
 
+  let canvas = document.getElementById("canvas");
 
-let canvas = document.getElementById("canvas");
+  let context = canvas.getContext("2d");
 
-let context = canvas.getContext("2d");
+  var window_height = window.innerHeight;
+  var window_width = window.innerWidth;
 
-var window_height = window.innerHeight;
-var window_width = window.innerWidth;
+  var left_beat = new Audio("https://github.com/ulazcano/emdr/raw/main/left_beat.wav");
+  var right_beat = new Audio("https://github.com/ulazcano/emdr/raw/main/right_beat.wav");
+  var speed_loop = obj.speed;
 
-var left_beat = new Audio("https://github.com/ulazcano/emdr/raw/main/left_beat.wav");
-var right_beat = new Audio("https://github.com/ulazcano/emdr/raw/main/right_beat.wav");
-var speed_loop = 60;
+  canvas.height = window_height;
+  canvas.width = window_width;
 
-canvas.height = window_height;
-canvas.width = window_width;
-
-canvas.style.background = "#ff8";
-
-// let loop_counter = 0;
-class Circle {
-  constructor(xpos, ypos, radius, color, text, speed) {
-    this.xpos = xpos;
-    this.ypos = ypos;
-    this.radius = radius;
-    this.color = color;
-    this.text = text;
-    this.speed = speed;
-    this.dx = 1 * this.speed;
-    this.dy = 0 * this.speed;
-  }
-  draw(context) {
-    context.beginPath();
-    /*    context.strokeStyle = this.color;
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    context.font = "20px Arial";
-    context.fillText(this.text, this.xpos, this.ypos);
-    context.lineWidth = 10;*/
-
-    context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
-    context.stroke();
-    context.fillStyle = "purple";
-    context.fill();
-
-    context.closePath();
-  }
-
-  update() {
+  canvas.style.background = obj.fondo;
+  let clear_canvas = function (){
     context.clearRect(0, 0, window_width, window_height);
-    this.draw(context);
-
-    if (this.xpos + this.radius > window_width) {
-      this.dx = -this.dx;
-      right_beat.play();
-    }
-    if (this.xpos - this.radius < 0) {
-      this.dx = -this.dx;
-      left_beat.play();
-      // loop_counter++;
-    }
-
-    this.xpos += this.dx;
-    this.ypos += this.dy;
   }
-}
+  // let loop_counter = 0;
+  class Circle {
+    constructor(xpos, ypos, radius, color, text, speed) {
+      this.xpos = xpos;
+      this.ypos = ypos;
+      this.radius = radius;
+      this.color = color;
+      this.text = text;
+      this.speed = speed;
+      this.dx = 1 * this.speed;
+      this.dy = 0 * this.speed;
+    }
+    draw(context) {
+      context.beginPath();
+      /*    context.strokeStyle = this.color;
+      context.textAlign = "center";
+      context.textBaseline = "middle";
+      context.font = "20px Arial";
+      context.fillText(this.text, this.xpos, this.ypos);
+      context.lineWidth = 10;*/
 
-let circle_counter = 1;
+      context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+      context.stroke();
+      context.fillStyle = obj.circulo;
+      context.fill();
 
-let my_circle = new Circle(window_width / 2, window_height / 2, 50, "black", circle_counter, speed_loop);
+      context.closePath();
+    }
 
-my_circle.draw(context);
+    update() {
+      clear_canvas();
+      //context.clearRect(0, 0, window_width, window_height);
+      this.draw(context);
 
-let updateCircle = function () {
-  requestAnimationFrame(updateCircle);
-  my_circle.update();
-};
+      if (this.xpos + this.radius > window_width) {
+        this.dx = -this.dx;
+        right_beat.play();
+      }
 
-updateCircle();
+      if (this.xpos - this.radius < 0) {
+        this.dx = -this.dx;
+        left_beat.play();
+        // loop_counter++;
+      }
+      
 
-/*
-for( var numbers = 0; numbers < 1; numbers++ ) {
+      this.xpos += this.dx;
+      this.ypos += this.dy;
+    }
+  }
+
+
+
+  console.log(obj.speed)
+  let my_circle = new Circle(window_width / 2, window_height / 2, 50, "black", 1, obj.speed);
+
+
+  function updateCircle(){
+    
+    my_circle.update();
+    if(update_switch == true){
+      requestAnimationFrame(updateCircle);
+    }
+    if(update_switch == false){
+      cancelAnimationFrame(updateCircle);
+  }
+  }
+
+  
+  // let updateCircle2 = function (cancel_animation) {
+  //   if(cancel_animation==1){
+  //     cancelAnimationFrame(asda);
+  //     console.log("entro en cancel")
+  //     return;}
+  //   var asda =requestAnimationFrame(updateCircle2);
+  //   asda;
+  //   my_circle.update();
+    
+    
+    
+
+  //  }
   
 
 
-  let my_circle = new Circle(random_x,random_y,50,"black",circle_counter,1);
-  all_circles.push(my_circle);
-  createCircle(my_circle);
-}
+
+    // let updateCircle = function (_switch) {
+    //   if(_switch==0){
+    //   updateCircle2();
+    //   }
+    //   if(_switch>0){
+    //     console.log(my_circle);
+    //     console.log("entro en 1");
+    //     clear_canvas();
+    //     my_circle = new Circle(window_width / 2, window_height / 2, 50, "black", 1, obj.speed);
+    //     javascript_abort();
+    //     alert("x")
+    //     updateCircle2();
+    //     console.log(my_circle);
+    //     }
+    // }
+  
+  
+  // let updateCircle = function (_switch) {
+  //   if(_switch==0){
+  //   updateCircle2();
+  //   }
+  //   if(_switch==1){
+  //     cancel_frame = 1;
+      
+      
+  //     clear_canvas();
+      
+      
+  //     }
+  // }
 
 
- 
-*/
+
+  
+
+
